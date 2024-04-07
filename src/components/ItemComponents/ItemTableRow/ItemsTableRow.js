26468d6722f3e151becd5ffcadcae55ff5de5b8d
Tabish Sheikh <150561947+shooterlittle12@users.noreply.github.com>
2024-04-07 13:30:28 -0300

    added an conformation dialog to delete the items

--- src/components/ItemComponents/ItemTableRow/ItemsTableRow.js
+++ src/components/ItemComponents/ItemTableRow/ItemsTableRow.js
@@ -1,27 +1,40 @@
-// ItemsTableRow.js
 import React from "react";
 import './ItemsTableRow.scss';
 
-const ItemsTableRow = ({ item, index, onEditItem, onDeleteItem }) => {
-    return(
-        <tr key={index}>
+
+
+
+// Row component 
+const ProductRow = ({ item, index, onEditItem, onDeleteItem }) => {
+    //edit action 
+    const handleEdit = () => onEditItem(item);
+
+
+
+
+    //delete action with a confirmation dialog
+    const confirmAndDelete = () => {
+        if (window.confirm('Delete, you sure?')) {
+            onDeleteItem(item);
+        }
+    };
+
+    return (
+        <tr>
             <td>{index + 1}</td>
             <td>{item.category_id}</td>
             <td>{item.title}</td>
             <td>{item.description}</td>
-            <td>{item.price}</td>
+            <td>${item.price}</td>
             <td>{item.quantity}</td>
             <td>{item.sku}</td>
             <td>
-                <button onClick={() => onEditItem(item)}>Edit</button>
-                <button onClick={() => {
-                    if (window.confirm('Delete, you sure?'))
-                        onDeleteItem(item);
-                    }
-                }>Delete</button>
+                <button onClick={handleEdit}>Edit</button>
+                <button onClick={confirmAndDelete}>Delete</button>
             </td>
         </tr>
     );
 };
 
-export default ItemsTableRow;
+export default ProductRow;
+
